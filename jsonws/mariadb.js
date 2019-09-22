a524d9bd741bb102a4a47ae0ccd91de5e88ccf80
crosfan <crosfan515@gmail.com>
2019-09-22 15:23:57 -0500

    Update mariadb.js

--- jsonws/mariadb.js
+++ jsonws/mariadb.js
@@ -1,34 +1,39 @@
 var express = require('express'); // Web Framework
 var app = express();
-var sql = require('mariadb'); // Maria DB client
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+     host: 'localhost', 
+     user:'myUser', 
+     password: 'myPassword',
+     connectionLimit: 5
+});
 
-// Connection string parameters.
-var sqlConfig = {
-    user: 'nodews',
-    password: '##########',
-    server: 'localhost',
-    database: 'webservice'
-}
 
 app.get('/', function (req, res) {
 	res.end('Nothing to see here, move along...');
 });
 
-app.get('/cb_divisions', function (req, res) {
 
-	new sql.ConnectionPool(sqlConfig).connect().then(pool => {
-	  return pool.request().query("select StateAbbreviation, StateName from states order by StateAbbreviation;")
-	  }).then(result => {
-		let rows = result.recordset
-		res.setHeader('Access-Control-Allow-Origin', '*')
-		res.status(200).json(rows);
-		sql.close();
-	  }).catch(err => {
-		res.status(500).send({ message: "${err}"})
-		sql.close();
-	  });
 
-})
+
+app.get('/states', function (req, res) {
+
+  let conn;
+  try {
+	conn = await pool.getConnection();
+	const rows = await conn.query("SELECT 1 as val");
+	console.log(rows); //[ {val: 1}, meta: ... ]
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.status(200).json(rows);
+
+  } catch (err) {
+	res.status(500).send({ message: "${err}"})
+	throw err;
+  } finally {
+	if (conn) return conn.end();
+  }
+});
+
 
 // Start server and listen on http://localhost:8080/
 var server = app.listen(8080, function () {
