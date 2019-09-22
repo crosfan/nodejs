77d78158d7695007fb959cf1d3f39db8f235b8c9
crosfan <crosfan515@gmail.com>
2019-09-22 13:32:19 -0500

    Update mssql.js

--- jsonws/mssql.js
+++ jsonws/mssql.js
@@ -1,1 +1,39 @@
-#
+var express = require('express'); // Web Framework
+var app = express();
+var sql = require('mssql'); // MS Sql Server client
+
+// Connection string parameters.
+var sqlConfig = {
+    user: 'nodejs',
+    password: '##########',
+    server: 'localhost',
+    database: 'JSON_WS'
+}
+
+app.get('/', function (req, res) {
+	res.end('Nothing to see here, move along...');
+});
+
+app.get('/cb_divisions', function (req, res) {
+
+	new sql.ConnectionPool(sqlConfig).connect().then(pool => {
+	  return pool.request().query("select DivisionMnemonic ,DivisionName from dbo.uvw_json_cb_divisions order by DivisionMnemonic")
+	  }).then(result => {
+		let rows = result.recordset
+		res.setHeader('Access-Control-Allow-Origin', '*')
+		res.status(200).json(rows);
+		sql.close();
+	  }).catch(err => {
+		res.status(500).send({ message: "${err}"})
+		sql.close();
+	  });
+
+})
+
+// Start server and listen on http://localhost:8091/
+var server = app.listen(8091, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("app listening at http://%s:%s", host, port)
+});
